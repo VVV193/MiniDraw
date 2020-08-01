753aa942781c3f43d69731c777a0e7191082187a
VVV <vvv193@mail.ru>
2020-08-01 08:12:21 +0300

    Описание класса CLine. Дополнения в классе документа.

--- MiniDraw/MiniDrawDoc.h
+++ MiniDraw/MiniDrawDoc.h
@@ -4,10 +4,30 @@
 
 
 #pragma once
-
+class CLine : public CObject
+{
+protected:
+	int m_X1, m_Y1, m_X2, m_Y2;
+public:
+	CLine(int X1, int Y1, int X2, int Y2)
+	{
+		m_X1 = X1;
+		m_Y1 = Y1;
+		m_X2 = X2;
+		m_Y2 = Y2;
+	}
+	void Draw(CDC *PDC);
+};
 
 class CMiniDrawDoc : public CDocument
 {
+protected:
+	CTypedPtrArray <CObArray, CLine*> m_LineArray;
+public:
+	void AddLine(int X1, int Y1, int X2, int Y2);
+	CLine *GetLine(int Index);
+	int GetNumLines();
+
 protected: // create from serialization only
 	CMiniDrawDoc() noexcept;
 	DECLARE_DYNCREATE(CMiniDrawDoc)
